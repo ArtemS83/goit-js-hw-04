1c7046fc5b06e1d8026151c99b1938b55c874f71
Atrem Sergieiev <a_serg83@ukr.net>
2020-10-14 13:45:45 +0200

    task-4_4

--- js/task-4_4.js
+++ js/task-4_4.js
@@ -1,1 +1,40 @@
+const account = {
+  owner: 'Mango',
+  balance: 24000,
+  discount: 0.1,
+  orders: ['order-1', 'order-2', 'order-3'],
+  changeDiscount(value) {
+    this.discount = value; // Write code in this line
+  },
+  showOrders() {
+    return this.orders; // Write code in this line
+  },
+  addOrder(cost, order) {
+    this.balance -= cost; // Write code in this line
+    this.orders.push(order); // Write code in this line
+  },
+};
+const copyAccount =  Object.assign({},account);
+copyAccount.orders = [...account.orders];
+// копируем для автотестов ссылочные типы
 
+
+account.changeDiscount(0.15);
+console.log(account.discount); // 0.15
+
+console.log(account.showOrders()); 
+// ['order-1', 'order-2', 'order-3']
+
+account.addOrder(5000, 'order-4');
+console.log(account.balance); // 19000
+
+console.log(account.showOrders());
+// ['order-1', 'order-2', 'order-3', 'order-4']
+
+//для примера Object.assign({}, obj)-копиррует(создает свой новый) обьект (а не ссылку на него)
+// var obj = { a: 1 };
+// var copy = Object.assign({}, obj);
+// console.log(copy); // { a: 1 }
+// console.log(copy === obj)//false
+// var copy = obj;
+// console.log(copy === obj)//true
